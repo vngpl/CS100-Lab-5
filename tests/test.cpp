9c389330d51d2d9901b8605bdaf6f6ac78d34bef
vngpl <v.ngpl1911@gmail.com>
2024-05-02 14:23:39 -0700

    finished passing tests for test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,2 +1,34 @@
 #include "gtest/gtest.h"
-#include "header/rectangle.hpp"
+#include "../header/rectangle.hpp"
+
+TEST(ConstructorTests, defaultConstructor) {
+    Rectangle rect;
+    EXPECT_EQ(rect.area(), 0);
+    EXPECT_EQ(rect.perimeter(), 0);
+}
+
+TEST(ConstructorTests, parametrizedConstructor) {
+    Rectangle rect(5, 10);
+    EXPECT_EQ(rect.area(), 50);
+    EXPECT_EQ(rect.perimeter(), 30);
+}
+
+TEST(AreaTests, zeroDimensions) {
+    Rectangle rect;
+    EXPECT_EQ(rect.area(), 0);
+}
+
+TEST(AreaTests, nonZeroDimensions) {
+    Rectangle rect(3, 4);
+    EXPECT_EQ(rect.area(), 12);
+}
+
+TEST(PerimeterTests, zeroDimensions) {
+    Rectangle rect;
+    EXPECT_EQ(rect.perimeter(), 0);
+}
+
+TEST(PerimeterTests, nonZeroDimensions) {
+    Rectangle rect(3, 4);
+    EXPECT_EQ(rect.perimeter(), 14);
+}
